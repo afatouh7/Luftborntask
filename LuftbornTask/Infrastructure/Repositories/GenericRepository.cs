7cd2c224e9bcfad04377f79f51024d5aacb29676
fatouh94 <Ahmed.Fatouh@onefinance.com.eg>
2025-05-16 02:43:52 +0300

    task init

---
+++ LuftbornTask/Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id) => await _dbSet.FindAsync(id);
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+            => await _dbSet.Where(predicate).ToListAsync();
+
+        public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+
+        public void Update(T entity)
+        {
+           
+            _dbSet.Update(entity);
+        }
+
+        public void Remove(T entity) => _dbSet.Remove(entity);
+
+        public async Task<int> CountAsync(Expression<Func<T, bool>> predicate = null)
+            => predicate == null ? await _dbSet.CountAsync() : await _dbSet.CountAsync(predicate);
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
+            => await _dbSet.AnyAsync(predicate);
+    }
+}
---
+++ LuftbornTask/LuftbornTask/Controllers/ProductsController.cs
@@ -0,0 +1,82 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LuftbornTask.Controllers
+{
+  //  [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetAll()
+        {
+            var products = await _productService.GetAllAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDto>> GetById(Guid id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+            return product != null ? Ok(product) : NotFound();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductDto>> Create(ProductDto productDto)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var createdProduct = await _productService.CreateAsync(productDto);
+            return CreatedAtAction(nameof(GetById), new { id = createdProduct.Id }, createdProduct);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, ProductDto productDto)
+        {
+            if (id != productDto.Id) return BadRequest("ID mismatch");
+
+            try
+            {
+                await _productService.UpdateAsync(productDto);
+                return NoContent();
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+                await _productService.DeleteAsync(id);
+                return NoContent();
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
